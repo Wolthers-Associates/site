1a345430d3509fbc16c27df14bfbac5018fa87fe
Danwoltrs <daniel@wolthers.com>
2025-06-24 21:04:32 -0300

    users

--- trips/js/main.js
+++ trips/js/main.js
@@ -1673,7 +1673,17 @@ function loadModalUsersList() {
                     <input type="checkbox" class="fluent-checkbox user-checkbox" data-user-id="${user.id}">
                 </td>
                 <td class="fluent-user-name">${user.name}</td>
-                <td class="fluent-user-email">${user.email}</td>
+                <td class="fluent-user-email">
+                    <div class="email-container">
+                        <span class="email-text">${user.email}</span>
+                        <button class="copy-email-btn" onclick="copyEmail('${user.email}')" title="Copy email">
+                            <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor">
+                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </td>
                 <td class="fluent-user-company">${getUserCompany(user)}</td>
                 <td>
                     <span class="fluent-badge-table ${user.role}">${getRoleDisplayName(user.role)}</span>
@@ -2574,7 +2584,17 @@ function renderFilteredUsers(users) {
                     <input type="checkbox" class="fluent-checkbox user-checkbox" data-user-id="${user.id}">
                 </td>
                 <td class="fluent-user-name">${user.name}</td>
-                <td class="fluent-user-email">${user.email}</td>
+                <td class="fluent-user-email">
+                    <div class="email-container">
+                        <span class="email-text">${user.email}</span>
+                        <button class="copy-email-btn" onclick="copyEmail('${user.email}')" title="Copy email">
+                            <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor">
+                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </td>
                 <td class="fluent-user-company">${getUserCompany(user)}</td>
                 <td>
                     <span class="fluent-badge-table ${user.role}">${getRoleDisplayName(user.role)}</span>
@@ -2626,5 +2646,15 @@ function updateSortIcons(activeColumn, direction) {
             icon.style.opacity = '0.5';
             icon.style.transform = 'rotate(0deg)';
         }
+    });
+}
+
+// Copy email to clipboard
+function copyEmail(email) {
+    navigator.clipboard.writeText(email).then(() => {
+        showToast(`Email ${email} copied to clipboard`, 'success');
+    }).catch(err => {
+        console.error('Failed to copy email:', err);
+        showToast('Failed to copy email', 'error');
     });
 } 
